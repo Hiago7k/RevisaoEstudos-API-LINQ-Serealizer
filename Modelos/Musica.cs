92f973f3385c387b8c30f5e643c37de5bfe0eb1d
Hiago 7k <hiago7k@gmail.com>
2026-04-06 20:07:45 -0300

    criando a classe, musica e realizando anotações da API e criando, método para exibir esses dados

---
+++ Modelos/Musica.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace RevisaoEstudos_API_LINQ_Serealizer.Modelos;
+
+internal class Musica
+{
+    [JsonPropertyName("song")]
+    public string? Nome {get;}
+    [JsonPropertyName("artist")]
+    public string? Artistia {get;}
+
+
+
+    public void ExibitDetalhesDaMusica() 
+    {
+        Console.WriteLine("Realizando requsição...");
+        Console.WriteLine($"- {Nome}");
+        Console.WriteLine($"- {Artistia}");
+    }
+}
