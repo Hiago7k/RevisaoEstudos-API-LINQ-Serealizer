4c712ab38245067aaf1d8d8c12b9d82637ded55f
Hiago 7k <hiago7k@gmail.com>
2026-04-07 18:35:19 -0300

    criando uma nova classe, onde vamos trabalhar com serelização

---
+++ Modelos/MusicasPreferidas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevisaoEstudos_API_LINQ_Serealizer.Modelos;
+
+internal class MusicasPreferidas
+{
+    public MusicasPreferidas(string? nome, List<Musica> listaDeMusicasFavoritas)
+    {
+        Nome = nome;
+        ListaDeMusicasFavoritas = new List<Musica>();
+    }
+
+    public string? Nome { get; set; }
+    public List<Musica> ListaDeMusicasFavoritas { get; }
+}
